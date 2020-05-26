ad801b92cda3a0e6a69adb391f838058a3f93a3d
Chris K <ikim1991@gmail.com>
2020-05-26 04:29:56 -0400

    Adding Functionality to Web Pages

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,7 +1,50 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import Profile from './assets/profile.jpg';
+import data from '../data/projects'
 
 class Projects extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      navigator: 1
+    }
+  }
+
+  createDots(){
+    const max = Object.keys(data).length
+    const content = []
+    for(let i = max; i > 0; i--){
+      if(this.state.navigator === (max - i) + 1){
+        content.push(<div className="dot" key={(max-i)+1} value={(max-i)+1} style={{backgroundImage: "linear-gradient(to bottom right,#ff8a00,#e52e71)"}}></div>)
+      } else{
+        content.push(<div className="dot" key={(max-i)+1} value={(max-i)+1} onClick={this.onDotClick}></div>)
+      }
+    }
+    return content
+  }
+
+  onDotClick = (e) => {
+    let i = parseInt(e.target.getAttribute("value"))
+    this.setState({ navigator: i })
+  }
+
+  onLeftClick = () => {
+    if(this.state.navigator !== 1){
+      this.setState((state) => {
+        return { navigator: state.navigator - 1 }
+      })
+    }
+  }
+
+  onRightClick = () => {
+    if(this.state.navigator !== Object.keys(data).length){
+      this.setState((state) => {
+        return { navigator: state.navigator + 1 }
+      })
+    }
+  }
+
   render(){
     return (
       <div>
@@ -11,10 +54,38 @@ class Projects extends React.Component{
             <h1>My Projects</h1>
           </div>
           <div className="main">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br/>
+            <div className="navigator w3-padding">
+              <i className="fa fa-arrow-left w3-xxlarge left-arrow" onClick={this.onLeftClick}></i>
+              {this.createDots()}
+              <i className="fa fa-arrow-right w3-xxlarge right-arrow" onClick={this.onRightClick}></i>
+            </div>
+            <div className="projects-content">
+              <div className="projects-grid-header">
+                <h3 className="w3-margin-bottom">{data[this.state.navigator].title}</h3>
+                <img src={`portfolio${data[this.state.navigator].url}`} alt="Profile" style={{width: 400, height:400, padding: 10}}/>
+              </div>
+              <div className="projects-grid-body">
+                <div>
+                  <h6>PROJECT SUMMARY</h6>
+                  <p>{data[this.state.navigator].summary}</p>
+                </div>
+                <div className="projects-list">
+                  <h6>TECHNOLOGIES AND FRAMEWORKS</h6>
+                  <ul>
+                    {data[this.state.navigator].technologies.map((li, ind) => {
+                      return (<li className="projects-technologies" key={ind}>{li}</li>)
+                    })}
+                  </ul>
+                </div>
+                <h6>LINKS</h6>
+                <div className="projects-links">
+                  <a href={data[this.state.navigator].github} target="_blank">[GitHub Repo]</a><br/>
+                  <a href={data[this.state.navigator].deployment} target="_blank">[Deployment]</a>
+                </div>
+              </div>
+            </div>
+
+
           </div>
           <div className="filler">
           </div>
---
+++ src/data/projects.js
@@ -0,0 +1,46 @@
+const data = {
+  1: {
+    title: "PROJECT TITLE 1",
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    technologies: [
+      "One",
+      "Two",
+      "Three",
+      "Four",
+      "Five"
+    ],
+    github: "https://google.ca",
+    deployment: "https://google.ca",
+    url: "/assets/profile.jpg"
+  },
+  2: {
+    title: "PROJECT TITLE 2",
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    technologies: [
+      "One",
+      "Two",
+      "Three",
+      "Four",
+      "Five"
+    ],
+    github: "https://google.ca",
+    deployment: "https://google.ca",
+    url: "/assets/profile.jpg"
+  },
+  3: {
+    title: "PROJECT TITLE 3",
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    technologies: [
+      "One",
+      "Two",
+      "Three",
+      "Four",
+      "Five"
+    ],
+    github: "https://google.ca",
+    deployment: "https://google.ca",
+    url: "/assets/profile.jpg"
+  },
+}
+
+export default data
