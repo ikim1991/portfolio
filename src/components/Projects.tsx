a64880c6762b20ec40eb1c1c997303a47c117dbf
Chris K <ikim1991@gmail.com>
2020-11-28 16:11:20 -0500

    Adding Project Logic

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
-import Image from '../assets/images/chess.jpg';
+import React, { useState } from 'react';
+import data from './data/projects.json';
 
 const Projects = () => {
+
+    const [slider, changeSlider] = useState(0);
+
+    const changeSliderRange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        changeSlider(parseInt(e.currentTarget.value))
+    }
+
+    const leftSlide = () => {
+        if(slider === 0){
+            return;
+        } else{
+            changeSlider(slider => slider - 1)
+        }
+    }
+
+    const rightSlide = () => {
+        if(slider === (data.projects.length - 3)){
+            return;
+        } else{
+            changeSlider(slider => slider + 1)
+        }
+    }
+
     return(
         <div className="main">
             <div className="main-header">
@@ -9,37 +32,31 @@ const Projects = () => {
             </div>
             <div className="projects-content">
                 <div className="projects">
-                    <div className="project">
-                        <h3>PROJECT TITLE</h3>
-                        <img src={Image} alt="project-icon"/>
-                        <div>
-                            <p>DEPLOYED</p>
-                            <p>REPOSITORY</p>
-                            <p>DOCUMENTATION</p>
-                        </div>
-                    </div>
-                    <div className="project">
-                        <h3>PROJECT TITLE</h3>
-                        <img src={Image} alt="project-icon"/>
-                        <div>
-                            <p>DEPLOYED</p>
-                            <p>REPOSITORY</p>
-                            <p>DOCUMENTATION</p>
-                        </div>
-                    </div>
-                    <div className="project">
-                        <h3>PROJECT TITLE</h3>
-                        <img src={Image} alt="project-icon"/>
-                        <div>
-                            <p>DEPLOYED</p>
-                            <p>REPOSITORY</p>
-                            <p>DOCUMENTATION</p>
-                        </div>
-                    </div>
+                    {data.projects.map((project, i) => {
+                        if((i === slider) || (i === slider+1) || (i === slider+2)){
+                            return(
+                                <div className="project" key={i}>
+                                    <h3>{project.title}</h3>
+                                    <div className="project-image">
+                                        <img src={project.imageUrl} alt="project-icon"/>
+                                    </div>
+                                    <div>
+                                        <p>Deployment: <a href={project.deployed} target="_blank"><i>Link Here</i></a></p>
+                                        <p>GitHub Repository: <a href={project.github} target="_blank"><i>Link Here</i></a></p>                                    
+                                        <p>Documentation: <a href={project.documentation} target="_blank"><i>Link Here</i></a></p>                                    
+                                    </div>
+                                </div>
+                            )
+                        } else{
+                            return false;
+                        }
+                    })}
                 </div>
             </div>
             <div className="project-range">
-                <input type="range" min="1" max="5" defaultValue="1" className="slider" id="projectRange"/>
+                <i className="fa fa-angle-left left-arrow" onClick={leftSlide}></i>
+                <input type="range" min="0" max={(data.projects.length - 3)} value={slider} className="slider" id="projectRange" onChange={changeSliderRange}/>
+                <i className="fa fa-angle-right right-arrow" onClick={rightSlide}></i>
             </div>
         </div>
     )
